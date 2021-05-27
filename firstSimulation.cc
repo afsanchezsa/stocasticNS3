6300fd779bc8a91d9d0f2dca2eb44b1f9853add6
afsanchezsa <afsanchezsa@unal.edu.co>
2021-05-27 13:36:08 -0700

    20 nodes in adhoc with udpserver and an udp client

--- firstSimulation.cc
+++ firstSimulation.cc
@@ -25,13 +25,13 @@
  // The simulation layout consists of N backbone routers interconnected
  // by an ad hoc wifi network.
  // Each backbone router also has a local 802.11 network and is connected
- // to a local LAN.  An additional set of (K-1) nodes are connected to
+ // to a local LAN.  An additional set of (K-1) backboneNodes are connected to
  // this backbone.  Finally, a local LAN is connected to each router
  // on the backbone, with L-1 additional hosts.
  //
- // The nodes are populated with TCP/IP stacks, and OLSR unicast routing
+ // The backboneNodes are populated with TCP/IP stacks, and OLSR unicast routing
  // on the backbone.  An example UDP transfer is shown.  The simulator
- // be configured to output tcpdumps or traces from different nodes.
+ // be configured to output tcpdumps or traces from different backboneNodes.
  //
  //
  //          +--------------------------------------------------------+
@@ -56,8 +56,19 @@
  // causing packets to traverse CSMA to adhoc to infrastructure links
  //
  // Note that certain mobility patterns may cause packet forwarding
- // to fail (if nodes become disconnected)
- 
+ // to fail (if backboneNodes become disconnected)
+#include "ns3/core-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/opengym-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/spectrum-module.h"
+#include "ns3/stats-module.h"
+#include "ns3/flow-monitor-module.h"
+#include "ns3/traffic-control-module.h"
+#include "ns3/node-list.h"
+
  #include "ns3/command-line.h"
  #include "ns3/string.h"
  #include "ns3/yans-wifi-helper.h"
@@ -73,6 +84,7 @@
  #include "ns3/csma-helper.h"
  #include "ns3/animation-interface.h"
  
+
  using namespace ns3;
  
  //
@@ -90,7 +102,11 @@
    Vector position = model->GetPosition ();
    std::cout << "CourseChange " << path << " x=" << position.x << ", y=" << position.y << ", z=" << position.z << std::endl;
  }
- 
+ uint64_t g_rxPktNum = 0;
+void DestRxPkt (Ptr<const Packet> packet)
+{     NS_LOG_UNCOND ("Client received a packet of " << packet->GetSize () << " bytes");
+  g_rxPktNum++;
+}
  int
  main (int argc, char *argv[])
  {
@@ -98,10 +114,11 @@
    // First, we declare and initialize a few local variables that control some
    // simulation parameters.
    //
-   uint32_t backboneNodes = 10;
-
-   uint32_t stopTime = 20;
+   uint32_t backboneNodes = 20;
+   uint32_t stopTime = 30;
    bool useCourseChangeCallback = false;
+   uint32_t pktPerSec = 30;
+   uint32_t payloadSize = 1500;
  
    //
    // Simulation defaults are typically set next, before command line
@@ -116,9 +133,9 @@
    // "--backboneNodes=20"
    //
    CommandLine cmd (__FILE__);
-   cmd.AddValue ("backboneNodes", "number of backbone nodes", backboneNodes);
-   //cmd.AddValue ("infraNodes", "number of leaf nodes", infraNodes);
-   //cmd.AddValue ("lanNodes", "number of LAN nodes", lanNodes);
+   cmd.AddValue ("backboneNodes", "number of backbone backboneNodes", backboneNodes);
+   //cmd.AddValue ("infraNodes", "number of leaf backboneNodes", infraNodes);
+   //cmd.AddValue ("lanNodes", "number of LAN backboneNodes", lanNodes);
    cmd.AddValue ("stopTime", "simulation stop time (seconds)", stopTime);
    cmd.AddValue ("useCourseChangeCallback", "whether to enable course change tracing", useCourseChangeCallback);
  
@@ -138,13 +155,13 @@
    //                                                                       //
  
    //
-   // Create a container to manage the nodes of the adhoc (backbone) network.
-   // Later we'll create the rest of the nodes we'll need.
+   // Create a container to manage the backboneNodes of the adhoc (backbone) network.
+   // Later we'll create the rest of the backboneNodes we'll need.
    //
    NodeContainer backbone;
    backbone.Create (backboneNodes);
    //
-   // Create the backbone wifi net devices and install them into the nodes in
+   // Create the backbone wifi net devices and install them into the backboneNodes in
    // our container
    //
    WifiHelper wifi;
@@ -159,11 +176,11 @@
    NetDeviceContainer backboneDevices = wifi.Install (wifiPhy, mac, backbone);
  
    // We enable OLSR (which will be consulted at a higher priority than
-   // the global routing) on the backbone ad hoc nodes
-   NS_LOG_INFO ("Enabling OLSR routing on all backbone nodes");
+   // the global routing) on the backbone ad hoc backboneNodes
+   NS_LOG_INFO ("Enabling OLSR routing on all backbone backboneNodes");
    OlsrHelper olsr;
    //
-   // Add the IPv4 protocol stack to the nodes in our container
+   // Add the IPv4 protocol stack to the backboneNodes in our container
    //
    InternetStackHelper internet;
    internet.SetRoutingHelper (olsr); // has effect on the next Install ()
@@ -178,8 +195,8 @@
    ipAddrs.Assign (backboneDevices);
  
    //
-   // The ad-hoc network nodes need a mobility model so we aggregate one to
-   // each of the nodes we just finished building.
+   // The ad-hoc network backboneNodes need a mobility model so we aggregate one to
+   // each of the backboneNodes we just finished building.
    //
    MobilityHelper mobility;
    mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
@@ -191,10 +208,72 @@
                                   "LayoutType", StringValue ("RowFirst"));
    mobility.SetMobilityModel ("ns3::RandomDirection2dMobilityModel",
                               "Bounds", RectangleValue (Rectangle (-500, 500, -500, 500)),
-                              "Speed", StringValue ("ns3::ConstantRandomVariable[Constant=2]"),
+                              "Speed", StringValue ("ns3::ConstantRandomVariable[Constant=0.2]"),
                               "Pause", StringValue ("ns3::ConstantRandomVariable[Constant=0.2]"));
    mobility.Install (backbone);
- 
+
+
+
+
+ /// recurso: https://github.com/xianliangjiang/NS3-Gym/blob/master/scratch/linear-mesh/sim.cc
+// Traffic
+  // Create a BulkSendApplication and install it on node 0
+  Ptr<UniformRandomVariable> startTimeRng = CreateObject<UniformRandomVariable> ();
+  startTimeRng->SetAttribute ("Min", DoubleValue (0.0));
+  startTimeRng->SetAttribute ("Max", DoubleValue (1.0));
+
+  uint16_t port = 1000;
+  uint32_t srcNodeId = 0;
+  uint32_t destNodeId = backbone.GetN() - 1;
+  Ptr<Node> srcNode = backbone.Get(srcNodeId);
+  Ptr<Node> dstNode = backbone.Get(destNodeId);
+
+  Ptr<Ipv4> destIpv4 = dstNode->GetObject<Ipv4> ();
+  Ipv4InterfaceAddress dest_ipv4_int_addr = destIpv4->GetAddress (1, 0);
+  Ipv4Address dest_ip_addr = dest_ipv4_int_addr.GetLocal ();
+
+  InetSocketAddress destAddress (dest_ip_addr, port);
+  destAddress.SetTos (0x70); //AC_BE
+  UdpClientHelper source (destAddress);
+  source.SetAttribute ("MaxPackets", UintegerValue (pktPerSec * stopTime));
+  source.SetAttribute ("PacketSize", UintegerValue (payloadSize));
+  Time interPacketInterval = Seconds (1.0/pktPerSec);
+  source.SetAttribute ("Interval", TimeValue (interPacketInterval)); //packets/s
+
+  ApplicationContainer sourceApps = source.Install (srcNode);
+  sourceApps.Start (Seconds (0.0));
+  sourceApps.Stop (Seconds (stopTime));
+
+  // Create a packet sink to receive these packets
+  UdpServerHelper sink (port);
+  ApplicationContainer sinkApps = sink.Install (dstNode);
+  sinkApps.Start (Seconds (0.0));
+  sinkApps.Stop (Seconds (stopTime));
+
+  Ptr<UdpServer> udpServer = DynamicCast<UdpServer>(sinkApps.Get(0));
+  udpServer->TraceConnectWithoutContext ("Rx", MakeCallback (&DestRxPkt));
+
+  // Print node positions
+  NS_LOG_UNCOND ("Node Positions:");
+  for (uint32_t i = 0; i < backbone.GetN(); i++)
+  {
+    Ptr<Node> node = backbone.Get(i);
+    Ptr<MobilityModel> mobility = node->GetObject<MobilityModel> ();
+    NS_LOG_UNCOND ("---Node ID: " << node->GetId() << " Positions: " << mobility->GetPosition());
+  }
+
+
+
+// fin recurso: https://github.com/xianliangjiang/NS3-Gym/blob/master/scratch/linear-mesh/sim.cc
+
+
+
+
+
+
+
+
+
    //                                                                       //
    // Tracing configuration                                                 //
    //                                                                       //
@@ -233,4 +312,5 @@
    Simulator::Stop (Seconds (stopTime));
    Simulator::Run ();
    Simulator::Destroy ();
+   
  }
